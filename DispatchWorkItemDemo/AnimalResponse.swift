c46cefc4cc95b026c019cae2133bab6e66f50053
MounikaMadishetti <madishettimounika123@gmail.com>
2021-07-24 08:56:35 -0700

    dispatch work item demo

---
+++ DispatchWorkItemDemo/AnimalResponse.swift
@@ -0,0 +1,8 @@
+//
+//  AnimalResponse.swift
+//  DispatchWorkItemDemo
+//
+//  Created by Mounika Madishetti on 24/07/21.
+//
+
+import Foundation
---
+++ DispatchWorkItemDemo/ServiceManager/ServiceManager.swift
@@ -0,0 +1,8 @@
+//
+//  ServiceManager.swift
+//  DispatchWorkItemDemo
+//
+//  Created by Mounika Madishetti on 24/07/21.
+//
+
+import Foundation
